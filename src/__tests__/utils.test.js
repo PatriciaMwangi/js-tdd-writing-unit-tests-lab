71745b08e6bf0f9de6bf776bbbb1375481b27824
patricia <patricia.mwangi@student.moringaschool.com>
2024-05-04 18:12:56 +0300

    ***

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,25 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome",()=>{
+    it("if the word reads the same forward and backward return true",()=>{
+        const input='racecar'
+        const inputed=isPalindrome(input)
+        expect(inputed).toBe(true);
+    });
+    it("return false if word is not palindrome",()=>{
+        const notpalindrome='car'
+        const notpalindromeword=isPalindrome(notpalindrome)
+        expect(notpalindromeword).toBe(false);
+    });
+ /*   it("returns true for words with a combination of uppercase and lowercase letters",()=>{
+        const combo="tEsT"
+        const combined=isPalindrome(combo)
+        expect(combined).toBe(true);
+    })*/
+   /* it("throws error for empty string",()=>{
+        const empty=''
+        const emptystring=isPalindrome(empty)
+        expect(emptystring).toBe(false)
+    })*/
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,12 @@
 // Your code here
+function reverse(string){
+    return string.split("").reverse().join("");
+}
+
+export function isPalindrome(word){
+if(!/^[A-Za-z]+$/.test(word)){
+    throw new Error('invalid');
+}
+const lowercase=word.toLowerCase()
+return lowercase===reverse(lowercase)
+}
